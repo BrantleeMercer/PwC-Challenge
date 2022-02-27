87e9ff8d006219e0b2f54773ab15fff73232b5a8
Brantlee Mercer <98775028+BrantleeMercer@users.noreply.github.com>
2022-02-26 19:40:01 -0500

    Grab interactions added, Starting Teleportation

--- Bow and Arrow/Assets/Scripts/TestScript.cs
+++ Bow and Arrow/Assets/Scripts/TestScript.cs
@@ -1,30 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
 
 public class TestScript : MonoBehaviour
 {
-    private ActionBasedController controller;
+    private InputDevice _rightController;
+    private InputDevice _leftController;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<ActionBasedController>();
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics right = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(right, devices);
 
-        controller.selectAction.action.performed += SelectActivated;
+        if(devices.Count > 0)
+        {
+            _rightController = devices[0];
+        } 
+
+        Debug.Log(_rightController.characteristics);      
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        _rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool rightPressed);
+        if(rightPressed)
+        {
+            Debug.Log("Right trigger pressed");
+        }
         
-    }
-
-    private void SelectActivated(InputAction.CallbackContext context)
-    {
-        Debug.Log("select button pressed");
     }
 }
