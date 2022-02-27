497433b8144ded863be602b5ae6e83f38e5bcee9
Brantlee Mercer <98775028+BrantleeMercer@users.noreply.github.com>
2022-02-26 23:21:54 -0500

    Arrow sockets onto bow string

--- Bow and Arrow/Assets/Scripts/TestScript.cs
+++ Bow and Arrow/Assets/Scripts/TestScript.cs
@@ -7,34 +7,20 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TestScript : MonoBehaviour
 {
-    private InputDevice _rightController;
-    private InputDevice _leftController;
 
     // Start is called before the first frame update
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();
-        InputDeviceCharacteristics right = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(right, devices);
-
-        if(devices.Count > 0)
-        {
-            _rightController = devices[0];
-        } 
-
-        Debug.Log(_rightController.characteristics);      
-
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool rightPressed);
-        if(rightPressed)
-        {
-            Debug.Log("Right trigger pressed");
-        }
-        
+
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log(other.gameObject.tag);
     }
 }
